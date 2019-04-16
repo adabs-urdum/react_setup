b60af8f1901b0d54d3fe4241f61e8fe527c8df35
adabs-urdum <cyrill.lehmann@clus.ch>
2019-04-16 10:29:26 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+
+/* jshint ignore:start */
+ReactDOM.render(<App />, document.getElementById('root'));
+/* jshint ignore:end */
